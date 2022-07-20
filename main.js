4c1a78698413d4bb9c35ae553d5bb80d5b5747bf
Khabibulix <unitetempete@gmail.com>
2022-07-20 18:15:59 +0200

    Adding moving functions

--- main.js
+++ main.js
@@ -1,38 +1,67 @@
 // CANVAS INIT
-// Const in Maj ;)
+
 const CANVAS = document.getElementById("game");
 const CTX = CANVAS.getContext("2d"); // Context, to use to draw in
+
+//CONST
 const X_CENTER = CANVAS.width / 2
 const Y_CENTER = CANVAS.height / 2
+const LEFT_ARROW_KEY = 37
+const RIGHT_ARROW_KEY = 38
+const UP_ARROW_KEY = 39
+const DOWN_ARROW_KEY = 40
+
 
 // INPUT MANAGER
 
 //document.addEventListener("mousedown", mouseListener, false); // Called when user interact with the mouse (usually on mouse click)
 //document.addEventListener("mousemove", mouseListener, false); // Called when the mouse is moving
+document.addEventListener("keydown", move, false)
+
+//Functions for input
 
 function mouseListener(event) // What to do then 
 {
     console.log(event);
 }
 
+function move(event){
+    console.log(event.key)
+}
+
+
+/**
+ * Represents a sprite.
+ * @constructor
+ * @param {Number} x_pos - The xPosition of the sprite.
+ * @param {Number} y_pos - The yPosition of the sprite.
+ * @param {Number} height - The height of the sprite.
+ * @param {Number} width - The width of the sprite.
+ */
 class Sprite {
-    constructor(setup) {
-        this.setup = setup
+    constructor(x_pos, y_pos, height, width) {
+        this.x_pos = x_pos
+        this.y_pos = y_pos
+        this.height = height
+        this.width = width     
     }
 
     draw(){
         CTX.fillStyle = 'red'
-        CTX.fillRect(this.setup.x, this.setup.y, this.setup.height, this.setup.width)
+        CTX.fillRect(this.x_pos, this.y_pos, this.height, this.width)
         return CTX
     }
 
-
+    move(){
+    }
 }
 
-const rectangle = new Sprite({x : 20,y : 20,width : 50,height : 50})
 
 
+const rectangle = new Sprite(20, 20, 50, 50)
 rectangle.draw();
+
+
 
 // MAIN
 
